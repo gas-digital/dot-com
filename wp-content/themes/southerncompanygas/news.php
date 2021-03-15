99a09bbc92cf01d259ac2370588aa9f3e03ac74d
barnesy <chris@barnesy.me>
2021-03-15 14:48:04 -0400

    Adds swap for category images

--- wp-content/themes/southerncompanygas/news.php
+++ wp-content/themes/southerncompanygas/news.php
@@ -30,9 +30,23 @@ $i = 0
     <?php get_template_part( 'templates/news-navigation' ); ?>
 
 		<!-- the loop -->
-		<?php while ( $wpb_all_query->have_posts() ) : $wpb_all_query->the_post(); ?>
+		<?php
+			while ( $wpb_all_query->have_posts() ) : $wpb_all_query->the_post();
+			$img = get_the_post_thumbnail_url();
+			$is_default = strpos($img, 'BG_Darkblue');
+			$cat = get_the_category();
+			$cat_name = $cat[0]->name;
+		?>
 				<a href="<?php the_permalink(); ?>" class="card <?php if(!$i) echo "card--mega"; $i++ ?>">
-					<div class="card--featured"><?= the_post_thumbnail( 'medium' ); ?></div>
+					<div class="card--featured">
+						<?php
+							if($is_default){
+								echo '<img src="/assets/img/news/' .  $cat_name. '.png" />';
+							}else {
+								echo the_post_thumbnail( 'medium' );
+							}
+						?>
+					</div>
 					<div class="card--content">
 						<p class="label"><?= the_date(); ?></p>
 						<p class="hed"><?php the_title(); ?></p>
